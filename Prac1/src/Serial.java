7d1e4a6898c8378d4895cc6902f8e569174db6a3
Shaaheen Sacoor <sacoorshaheen@gmail.com>
2015-07-31 09:27:52 +0200

    Added time measurements for comparisons

--- Prac1/src/Serial.java
+++ Prac1/src/Serial.java
@@ -1,6 +1,12 @@
 package src;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 
 /**
  * Created by Shaaheen on 7/28/2015.
@@ -8,21 +14,55 @@ import java.util.Arrays;
 public class Serial {
 
     //Array to test program on as input from text files hasn't been implemented yet
-    public static int[] testAry = {5,12,80,91,5,4,60,500,800,8,7,45,65,2,12,54,6,8,98,97,64,6,12,35,43,79,46,116,1};
+    public static double[] testAry = {5,12,80,91,5,4,60,500,800,8,7,45,65,2,12,54,6,8,98,97,64,6,12,35,43,79,46,116,1};
     public static int filterTest = 3;
 
-    public int[] noise;
+    public double[] noise;
     public int filter;
     public int ends;
 
-    Serial(int[] noiseArray){
+    Serial(double[] noiseArray){
         this.noise = noiseArray;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String workingDir = System.getProperty("user.dir");
+        double[] txt1 = FileUtil.getNoise(workingDir + "\\src\\TxtFiles\\inp1.txt");
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+
+        long time3 = System.currentTimeMillis();
+        System.out.println("Start : " + dateFormat.format(date));
+        Serial srTx1 = new Serial(txt1);
+        System.out.println( "Created Object : " + dateFormat.format(date));
+        srTx1.filterNoise(filterTest);
+        System.out.println("Done : " + dateFormat.format(date));
+        System.out.println("inp1.txt :");
+        long time4 = System.currentTimeMillis();
+        System.out.println("In Milli seconds : " + (time4 - time3));
+        //srTx1.printNoise();
+        System.out.println();
+        //System.out.println();
+
+        long time1 = System.currentTimeMillis();
+        double[] txt4 = FileUtil.getNoise(workingDir + "\\src\\TxtFiles\\inp4.txt");
+        System.out.println("Milli seconds :" + time1);
+        System.out.println("Start : " + dateFormat.format(date));
+        Serial srTx4 = new Serial(txt4);
+        System.out.println( "Created Object : " + dateFormat.format(date));
+        srTx4.filterNoise(filterTest);
+        System.out.println("Done : " + dateFormat.format(date));
+        long time2 = System.currentTimeMillis();
+        System.out.println("In Milliseconds : " + (time2 - time1));
+        System.out.println("inp1.txt :");
+        //srTx1.printNoise();
+        System.out.println();
+
         Serial sr1 = new Serial(testAry);
         sr1.filterNoise(filterTest);
         sr1.printNoise();
+        int[] testing = {5,6,8,2};
+        System.out.println(Arrays.toString(Arrays.copyOfRange(testing,1,testing.length-1)));
     }
 
     /*
@@ -37,7 +77,7 @@ public class Serial {
     /*
     Method to set values in an array to the median of the
     */
-    public void filterNoise(int filter){
+    public double[] filterNoise(int filter){
         this.filter = filter;
         this.ends = filter - ((filter/2) + 1); //Gets the border lengths i.e length from centre to end of array of filter size
         //eg filter size of 5 will give a segment {1,2,3,4,5} the ends = 2 as length between centre and end is 2
@@ -45,14 +85,15 @@ public class Serial {
         for (int i = ends; i < noise.length - ends; i++){
             noise[i] = getMiddle(i);
         }
+        return noise;
     }
 
     /*
         Method to get the median of a segment of an array at the index i
      */
-    public int getMiddle(int i){
+    public double getMiddle(int i){
 
-        int[] tempArray = Arrays.copyOfRange(noise , i - ends , i + ends + 1 );
+        double[] tempArray = Arrays.copyOfRange(noise , i - ends , i + ends + 1 );
         Arrays.sort(tempArray);
         return tempArray[tempArray.length/2];
     }
