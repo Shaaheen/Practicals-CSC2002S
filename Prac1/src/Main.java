4f53d921790c58876456421a47a1c1db1bf572f8
Shaaheen Sacoor <sacoorshaheen@gmail.com>
2015-08-05 20:28:37 +0200

    Created a UI class to showcase a comparison between the Parallel and Serial

--- Prac1/src/Main.java
+++ Prac1/src/Main.java
@@ -1,57 +1,76 @@
 package src;
 
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.Scanner;
 import java.util.concurrent.*;
 
 /**
  * Created by Shaaheen on 7/27/2015.
  */
 public class Main {
-    //Test array of what input text files would produce
-    public static int[] testAry = {5,12,80,91,5,4,60,500,800,8,7,45,65,2,12,54,6,8,98,97,64,6,12,35,43,79,46,116,1};
 
-    static final ForkJoinPool fjPool = new ForkJoinPool();
-    static int[] sumArray(int[] array,int i){
-        return fjPool.invoke(new TestThread(array,i));
-    }
+    //Gets current path to text files
+    public static String workingDir = System.getProperty("user.dir") + "\\src\\TxtFiles\\";
+    //Sets filter for when loading in files
+    public static int filter = 21;
 
-    public static void main(String[] args) throws InterruptedException{
-        //TestThread ts = new TestThread(5,6,8,1);
-        String time = "nfvjkf";
-        int t = 06;
-        String yess = "Hello " + "World";
-        //int yu = Integer.parseInt(yess.substring(0,1) + yess.substring(1,2));
+    public static void main(String[] args) throws InterruptedException, IOException {
 
-        //System.out.println(ts.ans);
-        for (int i = 0; i <  testAry.length - 5 ; i+=5){
-            sumArray(Arrays.copyOfRange(testAry,i,i+5),i);
+        //Scanner Object
+        Scanner user_input = new Scanner(System.in);
+        String option;
+
+        //UI loop
+        while (true){
+            //Options for user
+            System.out.println("Choose Input File" +
+                    "\r\n1)inp1.txt" +
+                    "\r\n2)inp2.txt" +
+                    "\r\n3)inp3.txt" +
+                    "\r\n4)inp4.tx" +
+                    "\r\n5)Change Filter (Filter is " + filter + ")" +
+                    "\r\n6)Quit");
+
+            option = user_input.nextLine();
+            //Create Parallel and Serial class to compare
+            if (option.equals("1")){
+                //Create and set Parallel class with name of file of array and filter
+                RecursiveThread RT1 = new RecursiveThread(workingDir + "inp1.txt",filter);
+                System.out.println("Parallel : " + RT1.parallelNoiseFilter());
+                Serial sr = new Serial(workingDir  + "inp1.txt",filter);
+                System.out.println("Sequential : " + sr.serialNoiseFilter());
+            }
+            else if (option.equals("2")){
+                RecursiveThread RT2 = new RecursiveThread(workingDir + "inp2.txt",filter);
+                System.out.println("Parallel : " + RT2.parallelNoiseFilter());
+                Serial sr = new Serial(workingDir  + "inp2.txt",filter);
+                System.out.println("Sequential : " + sr.serialNoiseFilter());
+            }
+            else if (option.equals("3")){
+                RecursiveThread RT3 = new RecursiveThread(workingDir + "inp3.txt",filter);
+                System.out.println("Parallel : " + RT3.parallelNoiseFilter());
+                Serial sr = new Serial(workingDir  + "inp3.txt",filter);
+                System.out.println("Sequential : " + sr.serialNoiseFilter());
+            }
+            else if (option.equals("4")){
+                RecursiveThread RT4 = new RecursiveThread(workingDir + "inp4.txt",filter);
+                System.out.println("Parallel : " + RT4.parallelNoiseFilter());
+                Serial sr = new Serial(workingDir  + "inp4.txt",filter);
+                System.out.println("Sequential : " + sr.serialNoiseFilter());
+            }
+            //Changes filter for arrays
+            else if (option.equals("5")){
+                System.out.println("Choose new Filter :");
+                filter = user_input.nextInt();
+                user_input.nextLine();
+            }
+            else{
+                System.exit(0);
+            }
+
+
         }
 
     }
-}
-
-//To test threading
-class TestThread extends  RecursiveTask<int[]>{
-    //Instance var to store object properties
-    public int threadNum;
-    public int[] splitAry;
-    public Serial sequential;
-
-    //Constructor method to set object variables
-    TestThread(int[] arry,int i){
-        sequential = new Serial(arry);
-        this.splitAry = arry;
-        this.threadNum = i;
-    }
-
-    //Thread method that will run when thread initialised
-    public int[] compute(){
-        //Arrays.sort(splitAry);
-        //System.out.println("Thread " + threadNum + " Median is: " + splitAry[splitAry.length/2]);
-        sequential.filterNoise(3);
-        sequential.printNoise();
-        System.out.println("Thread " + threadNum);
-        return null;
-    }
-
 }
