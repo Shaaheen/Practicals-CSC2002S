47fa34cd7b2bc27a05665dd998b01f50ec4c5d09
Shaaheen Sacoor <sacoorshaheen@gmail.com>
2015-09-01 22:04:16 +0200

    Added images to resource folder

--- Prac3/app/src/main/java/me/shaaheen/prac3/MainActivity.java
+++ Prac3/app/src/main/java/me/shaaheen/prac3/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Gallery;
 import android.widget.ImageView;
 import android.widget.ViewFlipper;
 
@@ -16,18 +17,22 @@ public class MainActivity extends Activity {
 
     private int keepCount;
 
-    private int[] images = {R.drawable.economics,R.drawable.fitzpatrick,R.drawable.snape};
+    private int[] images = {R.drawable.economics,R.drawable.fitzpatrick,R.drawable.snape,
+            R.drawable.editedct,R.drawable.betterfromfield,R.drawable.editeduct10};
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         viewFlipper = (ViewFlipper) findViewById(R.id.viewFlipper1);
 
         ImageView testImg = new ImageView(this);
         testImg.setImageResource(images[keepCount]);
+        testImg.willNotCacheDrawing();
         viewFlipper.addView(testImg);
+        viewFlipper.willNotCacheDrawing();
 
     }
 
@@ -57,26 +62,26 @@ public class MainActivity extends Activity {
 
                     System.out.println("Here atleast keepCount " + keepCount + "images length " + images.length);
 
-                    if (keepCount > 0){
-                        System.out.println("here keepCount" + keepCount + "  views" + viewFlipper.toString());
-                        viewFlipper.removeAllViews();
-                        ImageView newImg = new ImageView(this);
-                        newImg.setImageResource(images[keepCount]);
-                        viewFlipper.addView(newImg);
-                        //viewFlipper.setInAnimation(this,R.anim.slide_in_from_left);
-                        //viewFlipper.setInAnimation(this,R.anim.slide_out_from_right);
-                        viewFlipper.showNext();
-                    }
-                    else{
+                    if (keepCount < 0){
                         keepCount = images.length - 1;
-                        viewFlipper.removeAllViews();
-                        ImageView newImg = new ImageView(this);
-                        newImg.setImageResource(images[keepCount]);
-                        viewFlipper.addView(newImg);
-                        //viewFlipper.setInAnimation(this,R.anim.slide_in_from_left);
-                        //viewFlipper.setInAnimation(this,R.anim.slide_out_from_right);
-                        viewFlipper.showNext();
                     }
+
+                    System.out.println("here keepCount" + keepCount + "  views" + viewFlipper.toString());
+                    viewFlipper.removeAllViews();
+                    viewFlipper.removeAllViewsInLayout();
+                    viewFlipper.refreshDrawableState();
+
+                    ImageView newImg = new ImageView(this);
+                    newImg.setImageResource(images[keepCount]);
+                    newImg.setScaleType(ImageView.ScaleType.FIT_XY);
+                    newImg.willNotCacheDrawing();
+
+                    viewFlipper.addView(newImg);
+                    //viewFlipper.setInAnimation(this,R.anim.slide_in_from_left);
+                    //viewFlipper.setInAnimation(this,R.anim.slide_out_from_right);
+                    viewFlipper.showNext();
+                    onTrimMemory(60);
+
 
 
                 }
@@ -86,27 +91,26 @@ public class MainActivity extends Activity {
                     }
 
                     keepCount++;
-                    System.out.println("Here atlease keepCount " + keepCount);
+                    System.out.println("Here at least, keepCount " + keepCount);
 
-                    if (keepCount < images.length ){
-                        viewFlipper.removeAllViews();
-                        ImageView newImg = new ImageView(this);
-                        newImg.setImageResource(images[keepCount]);
-                        viewFlipper.addView(newImg);
-                        //viewFlipper.setInAnimation(this,R.anim.slide_in_from_left);
-                        //viewFlipper.setInAnimation(this,R.anim.slide_out_from_right);
-                        viewFlipper.showNext();
-                    }
-                    else {
+                    if (keepCount >= images.length ){
                         keepCount = 0;
-                        viewFlipper.removeAllViews();
-                        ImageView newImg = new ImageView(this);
-                        newImg.setImageResource(images[keepCount]);
-                        viewFlipper.addView(newImg);
-                        //viewFlipper.setInAnimation(this,R.anim.slide_in_from_left);
-                        //viewFlipper.setInAnimation(this,R.anim.slide_out_from_right);
-                        viewFlipper.showNext();
                     }
+
+                    viewFlipper.removeAllViews();
+                    viewFlipper.removeAllViewsInLayout();
+                    viewFlipper.refreshDrawableState();
+                    onTrimMemory(5);
+                    ImageView newImg = new ImageView(this);
+                    newImg.setImageResource(images[keepCount]);
+                    newImg.setScaleType(ImageView.ScaleType.FIT_XY);
+                    newImg.willNotCacheDrawing();
+                    viewFlipper.addView(newImg);
+                    //viewFlipper.setInAnimation(this,R.anim.slide_in_from_left);
+                    //viewFlipper.setInAnimation(this,R.anim.slide_out_from_right);
+                    viewFlipper.showNext();
+                    onTrimMemory(60);
+
                 }
                 break;
 
