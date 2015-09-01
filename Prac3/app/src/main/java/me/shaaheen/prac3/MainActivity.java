fd7edf0ec50bc039808f62b9b4df891af55b9da7
Shaaheen Sacoor <sacoorshaheen@gmail.com>
2015-09-01 15:26:21 +0200

    Added functionailty to shift pictures when a person swipes the screen

--- Prac3/app/src/main/java/me/shaaheen/prac3/MainActivity.java
+++ Prac3/app/src/main/java/me/shaaheen/prac3/MainActivity.java
@@ -1,37 +1,117 @@
 package me.shaaheen.prac3;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private ViewFlipper viewFlipper;
+    private float lastX;
+    private View  lastView;
+    private View nextView;
+
+    private int keepCount;
+
+    private int[] images = {R.drawable.economics,R.drawable.fitzpatrick,R.drawable.snape};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewFlipper = (ViewFlipper) findViewById(R.id.viewFlipper1);
+
+        ImageView testImg = new ImageView(this);
+        testImg.setImageResource(images[keepCount]);
+        viewFlipper.addView(testImg);
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public boolean onTouchEvent(MotionEvent touchEvent){
+        System.out.println("Touched");
+        switch (touchEvent.getAction()){
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+            case MotionEvent.ACTION_DOWN:
+                lastX = touchEvent.getX();
+                System.out.println("Init touch");
+                break;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+
+            case MotionEvent.ACTION_UP:
+                System.out.println("Swipe");
+                float currentX = touchEvent.getX();
+
+                //Left to Right Swipe
+                if (lastX < currentX){
+
+                    //If at edge of screen then can't swipe right
+                    if (viewFlipper.getDisplayedChild() == 0){
+                        //break;
+                    }
+
+                    keepCount--;
+
+                    System.out.println("Here atleast keepCount " + keepCount + "images length " + images.length);
+
+                    if (keepCount > 0){
+                        System.out.println("here keepCount" + keepCount + "  views" + viewFlipper.toString());
+                        viewFlipper.removeAllViews();
+                        ImageView newImg = new ImageView(this);
+                        newImg.setImageResource(images[keepCount]);
+                        viewFlipper.addView(newImg);
+                        //viewFlipper.setInAnimation(this,R.anim.slide_in_from_left);
+                        //viewFlipper.setInAnimation(this,R.anim.slide_out_from_right);
+                        viewFlipper.showNext();
+                    }
+                    else{
+                        keepCount = images.length - 1;
+                        viewFlipper.removeAllViews();
+                        ImageView newImg = new ImageView(this);
+                        newImg.setImageResource(images[keepCount]);
+                        viewFlipper.addView(newImg);
+                        //viewFlipper.setInAnimation(this,R.anim.slide_in_from_left);
+                        //viewFlipper.setInAnimation(this,R.anim.slide_out_from_right);
+                        viewFlipper.showNext();
+                    }
+
+
+                }
+                if (lastX > currentX){
+                    if (viewFlipper.getDisplayedChild()  == 1){
+                        //break;
+                    }
+
+                    keepCount++;
+                    System.out.println("Here atlease keepCount " + keepCount);
+
+                    if (keepCount < images.length ){
+                        viewFlipper.removeAllViews();
+                        ImageView newImg = new ImageView(this);
+                        newImg.setImageResource(images[keepCount]);
+                        viewFlipper.addView(newImg);
+                        //viewFlipper.setInAnimation(this,R.anim.slide_in_from_left);
+                        //viewFlipper.setInAnimation(this,R.anim.slide_out_from_right);
+                        viewFlipper.showNext();
+                    }
+                    else {
+                        keepCount = 0;
+                        viewFlipper.removeAllViews();
+                        ImageView newImg = new ImageView(this);
+                        newImg.setImageResource(images[keepCount]);
+                        viewFlipper.addView(newImg);
+                        //viewFlipper.setInAnimation(this,R.anim.slide_in_from_left);
+                        //viewFlipper.setInAnimation(this,R.anim.slide_out_from_right);
+                        viewFlipper.showNext();
+                    }
+                }
+                break;
+
+
         }
-
-        return super.onOptionsItemSelected(item);
+        return false;
     }
 }
