ff81bfe6c806868904a718c82434db51cfebe81b
Shaaheen Sacoor <sacoorshaheen@gmail.com>
2015-09-02 09:26:47 +0200

    Implementing ViewPager as opposed to ViewFlipper to save on memory with pictures

--- Prac3/app/src/main/java/me/shaaheen/prac3/SlideShow.java
+++ Prac3/app/src/main/java/me/shaaheen/prac3/SlideShow.java
@@ -1,7 +1,41 @@
 package me.shaaheen.prac3;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+
 /**
  * Created by Shaaheen on 9/2/2015.
  */
-public class SlideShow {
+public class SlideShow extends FragmentActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.slideshow);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPager.setAdapter(new CustomPagerAdapter(getSupportFragmentManager()));
+    }
+
+    private class CustomPagerAdapter extends FragmentPagerAdapter {
+
+        public CustomPagerAdapter(FragmentManager supportFragmentManager) {
+            super(supportFragmentManager);
+        }
+
+        @Override
+        public android.support.v4.app.Fragment getItem(int position) {
+
+        }
+
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+    }
 }
--- Prac3/app/src/main/java/me/shaaheen/prac3/StartMenu.java
+++ Prac3/app/src/main/java/me/shaaheen/prac3/StartMenu.java
@@ -1,7 +1,37 @@
 package me.shaaheen.prac3;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
+
 /**
  * Created by Shaaheen on 9/2/2015.
  */
-public class StartMenu {
+public class StartMenu extends Activity{
+
+    Button welcomeButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_menu);
+
+        welcomeButton = (Button) findViewById(R.id.welcome_button);
+        welcomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartMenu.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+
+
+
 }
