242e5a1179f06dbc05502a1d86a47d98cdd692ba
Shaaheen Sacoor <sacoorshaheen@gmail.com>
2015-09-03 15:43:47 +0200

    Changed intro picture to custom image and added slide show functionality with Runnable class

--- Prac3/app/src/main/java/me/shaaheen/prac3/StartMenu.java
+++ Prac3/app/src/main/java/me/shaaheen/prac3/StartMenu.java
@@ -2,11 +2,18 @@ package me.shaaheen.prac3;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ViewFlipper;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 /**
  * Created by Shaaheen on 9/2/2015.
@@ -14,6 +21,8 @@ import android.widget.ViewFlipper;
 public class StartMenu extends Activity{
 
     Button welcomeButton;
+    ImageView imageView;
+    int counter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +30,20 @@ public class StartMenu extends Activity{
         setContentView(R.layout.start_menu);
 
         welcomeButton = (Button) findViewById(R.id.welcome_button);
+        imageView = (ImageView) findViewById(R.id.mainImage);
+
+        imageView.setImageResource(R.drawable.intro);
+        counter = 0;
+
         welcomeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(StartMenu.this,MainActivity.class);
+                Intent intent = new Intent(StartMenu.this,SlideShow.class);
                 startActivity(intent);
                 finish();
             }
         });
+
 
     }
 
