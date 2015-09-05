6d89c7db0e22e1f834be8119f9313b9492fa5b73
Shaaheen Sacoor <sacoorshaheen@gmail.com>
2015-09-05 17:38:11 +0200

    Will save all images in drawable into the internal storage of phone if not already there, then will load in those images in internal memory to display in slideshow

--- Prac3/app/src/main/java/me/shaaheen/prac3/SlideFragment.java
+++ Prac3/app/src/main/java/me/shaaheen/prac3/SlideFragment.java
@@ -1,109 +1,148 @@
 package me.shaaheen.prac3;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 /**
  * Created by Shaaheen on 9/2/2015.
+ * Class that represents the actual Slide object containing the actual image with text attached
  */
 public class SlideFragment extends android.support.v4.app.Fragment{
 
+    static String appDirectory ;
+
+    /*
+        Method to overide parent class  - launched when new slide is chosen
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, final ViewGroup container, Bundle savedInstanceState){
+        //Get view and set layout from xml
         View v = inflater.inflate(R.layout.slide_fragment,container,false);
+        //Get the relative layout and set to black  - background color = black
         RelativeLayout relativeLayout = (RelativeLayout) v.findViewById(R.id.mainLayout);
+        relativeLayout.setBackgroundColor(Color.BLACK);
 
+        //Get text displaying object
+        TextView textView = (TextView) v.findViewById(R.id.imageDescription);
+
+        //Grab the main image view of the slide
         ImageView imageView = (ImageView) v.findViewById(R.id.currentSlide);
-        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+        imageView.setScaleType(ImageView.ScaleType.FIT_XY); //So slide fills whole screen
+        //Prevents too many picture stored on memory
         imageView.destroyDrawingCache();
         imageView.refreshDrawableState();
 
-        relativeLayout.setBackgroundColor(Color.BLACK);
+        appDirectory = Environment.getExternalStorageDirectory().getAbsolutePath() +  "/DiscoverUCT";
 
-        System.out.println("Message is " + getArguments().getString("msg"));
-
+        //Set the image and text depending on position given
         if (getArguments().getString("msg").equals("1")){
-            imageView.setImageResource(R.drawable.smalltable);
+            //Create bitmap from the image saved in the internal storage
+            Bitmap bmp = BitmapFactory.decodeFile(appDirectory + "/TableMnt.jpeg");
+            imageView.setImageBitmap(bmp);
+            //imageView.setImageResource(R.drawable.smalltable);
+            textView.setText("View of table mountain from Lower Campus");
         }
         else if (getArguments().getString("msg").equals("2")){
-            imageView.setImageResource(R.drawable.smalljammie);
+            //Create bitmap from the image saved in the internal storage
+            Bitmap bmp = BitmapFactory.decodeFile(appDirectory + "/Jammie.jpeg");
+            imageView.setImageBitmap(bmp);
+            textView.setText("Jameson Hall - The centre of the university");
         }
         else if (getArguments().getString("msg").equals("3")){
-            imageView.setImageResource(R.drawable.smallfitz);
+            //Create bitmap from the image saved in the internal storage
+            Bitmap bmp = BitmapFactory.decodeFile(appDirectory + "/CloseUp.jpeg");
+            imageView.setImageBitmap(bmp);
+            textView.setText("Close-up of a UCT building");
+            textView.setTextColor(Color.WHITE);
         }
         else if (getArguments().getString("msg").equals("4")){
-            imageView.setImageResource(R.drawable.smalcsc);
+            //Create bitmap from the image saved in the internal storage
+            Bitmap bmp = BitmapFactory.decodeFile(appDirectory + "/CSC.jpeg");
+            imageView.setImageBitmap(bmp);
+            textView.setText("Computer Science Senior Labs!");
         }
         else if (getArguments().getString("msg").equals("5")){
-            imageView.setImageResource(R.drawable.smallct);
+            //Create bitmap from the image saved in the internal storage
+            Bitmap bmp = BitmapFactory.decodeFile(appDirectory + "/CT.jpeg");
+            imageView.setImageBitmap(bmp);
+            textView.setText("The city you would be living in :)");
+            textView.setTextColor(Color.WHITE);
+
         }
         else if (getArguments().getString("msg").equals("6")){
-            imageView.setImageResource(R.drawable.smalltuggies);
+            //Create bitmap from the image saved in the internal storage
+            Bitmap bmp = BitmapFactory.decodeFile(appDirectory + "/Lower.jpeg");
+            imageView.setImageBitmap(bmp);
+            textView.setText("Night view of lower campus");
+            textView.setTextColor(Color.WHITE);
         }
         else if (getArguments().getString("msg").equals("7")){
-            imageView.setImageResource(R.drawable.smallgreenuct);
+            //Create bitmap from the image saved in the internal storage
+            Bitmap bmp = BitmapFactory.decodeFile(appDirectory + "/GreenUCT.jpeg");
+            imageView.setImageBitmap(bmp);
+            textView.setText("Upper campus when its blooming");
+            textView.setTextColor(Color.WHITE);
         }
         else if (getArguments().getString("msg").equals("8")){
-            imageView.setImageResource(R.drawable.smallarts);
+            //Create bitmap from the image saved in the internal storage
+            Bitmap bmp = BitmapFactory.decodeFile(appDirectory + "/Arts.jpeg");
+            imageView.setImageBitmap(bmp);
+            textView.setText("The UCT Arts building ");
+            textView.setTextColor(Color.WHITE);
         }
         else if (getArguments().getString("msg").equals("9")){
-            imageView.setImageResource(R.drawable.smallsnape);
+            //Create bitmap from the image saved in the internal storage
+            Bitmap bmp = BitmapFactory.decodeFile(appDirectory + "/Snape.jpeg");
+            imageView.setImageBitmap(bmp);
+            textView.setText("The new High Tech Engineering building");
+            textView.setTextColor(Color.WHITE);
         }
         else if (getArguments().getString("msg").equals("10")){
-            imageView.setImageResource(R.drawable.smallsnapeagain);
+            //Create bitmap from the image saved in the internal storage
+            Bitmap bmp = BitmapFactory.decodeFile(appDirectory + "/Snape2.jpeg");
+            imageView.setImageBitmap(bmp);
+            textView.setText("Another view from inside the engineeing building");
+            textView.setTextColor(Color.WHITE);
         }
         else if (getArguments().getString("msg").equals("11")){
-            imageView.setImageResource(R.drawable.finalslide);
+            //Create bitmap from the image saved in the internal storage
+            Bitmap bmp = BitmapFactory.decodeFile(appDirectory + "/FinalSlide.jpeg");
+            imageView.setImageBitmap(bmp);
+            //textView.setText("Applications open!");
         }
         else{
-            imageView.setImageResource(R.drawable.smallesteco);
+            Bitmap bmp = BitmapFactory.decodeFile(appDirectory + "/Eco.jpeg");
+            imageView.setImageBitmap(bmp);
+            textView.setText("Passage way past the Economics building..");
+            textView.setTextColor(Color.WHITE);
         }
-
-        /*
-        if (getArguments().getString("msg").equals("1")){
-            imageView.setImageResource(R.drawable.snape);
-        }
-        else if (getArguments().getString("msg").equals("2")){
-            imageView.setImageResource(R.drawable.editeductsmall);
-        }
-        else if (getArguments().getString("msg").equals("3")){
-            imageView.setImageResource(R.drawable.snapeagain);
-        }
-        else if (getArguments().getString("msg").equals("4")){
-            imageView.setImageResource(R.drawable.fitzpatrick);
-        }
-        else if (getArguments().getString("msg").equals("5")){
-            imageView.setImageResource(R.drawable.upper);
-        }
-        else if (getArguments().getString("msg").equals("6")){
-            imageView.setImageResource(R.drawable.tuggies);
-        }
-        else{
-            imageView.setImageResource(R.drawable.small);
-        }
-        */
-
         return v;
     }
 
+    /*
+        Method to create a new slide and set image and text of the slide
+     */
     public static SlideFragment newInstance(int position) {
 
         SlideFragment f = new SlideFragment();
-        System.out.println("new Instance yay!");
 
+        //For bundle that will be passed to slide
         Bundle b = new Bundle();
-        b.putString("msg", position + "");
+        b.putString("msg", position + ""); //Store position variable in bundle so to access to know which image to use
 
-        f.setArguments(b);
+        f.setArguments(b); //Sets image and text
 
-        System.out.println("Done with Instance");
-
+        //Return slide with all information attached
         return f;
     }
 }
--- Prac3/app/src/main/java/me/shaaheen/prac3/StartMenu.java
+++ Prac3/app/src/main/java/me/shaaheen/prac3/StartMenu.java
@@ -1,52 +1,154 @@
 package me.shaaheen.prac3;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 import android.widget.ViewFlipper;
 
 import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 /**
  * Created by Shaaheen on 9/2/2015.
+ * Class that manages the start screen and its functionality
  */
 public class StartMenu extends Activity{
 
-    Button welcomeButton;
-    ImageView imageView;
-    int counter;
 
+    //Instance variable
+    Button welcomeButton; //Button that launches app into view pager mode
+    ImageView imageView; //For background image
+    static int[] images = new int[]{R.drawable.smallesteco,R.drawable.smallct,R.drawable.smalcsc,R.drawable.smallarts,
+                                    R.drawable.finalslide,R.drawable.intro,R.drawable.smallfitz,R.drawable.smallgreenuct,
+                                    R.drawable.smalljammie,R.drawable.smallsnape,R.drawable.smallsnapeagain,R.drawable.smalltable,
+                                    R.drawable.smalltuggies};
+    static String[] imageFileNames = new String[]{"Eco.jpeg","CT.jpeg","CSC.jpeg","Arts.jpeg","FinalSlide.jpeg","Intro.jpeg",
+            "CloseUp.jpeg","GreenUCT.jpeg","Jammie.jpeg","Snape.jpeg","Snape2.jpeg","TableMnt.jpeg","Lower.jpeg"};
+
+    /*
+        Method to set layout and give button functionality
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Sets default create method
         super.onCreate(savedInstanceState);
+        //Sets xml as the layout
         setContentView(R.layout.start_menu);
 
+        //Get button object and image view object
         welcomeButton = (Button) findViewById(R.id.welcome_button);
         imageView = (ImageView) findViewById(R.id.mainImage);
 
+        //Set the introduction start page image
         imageView.setImageResource(R.drawable.intro);
-        counter = 0;
 
+        try {
+            saveImagesIntoMemory();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //Give functionality to the welcome button to launch into the view pager
         welcomeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Create a new intent with the intention of switching classes/views
                 Intent intent = new Intent(StartMenu.this,SlideShow.class);
-                startActivity(intent);
-                finish();
+                startActivity(intent); //Switch to SlideShow class
+                finish(); //End current class appriopiately
             }
         });
 
 
     }
 
+    public void saveImagesIntoMemory() throws IOException {
+        if (checkForMemoryCard()!=null){
+            System.out.println("SD card found again");
+            saveFiles();
+            System.out.println("Saved images!");
+        }
+    }
+
+    public void saveFiles() throws IOException {
+        String targetPath = Environment.getExternalStorageDirectory().getAbsolutePath() +  "/DiscoverUCT";
+        System.out.println("Target Path is: " + targetPath);
+
+        OutputStream fOut = null;
+        //To check if directory "DiscoverUCT/" already exists
+        File folder = new File(targetPath);
+
+        //If doesn't
+        if (!folder.exists()) {
+            folder.mkdir(); //Create directory
+            System.out.println("Folder not found - now created");
+        }
+        else{
+            System.out.println("Folder found");
+        }
+
+        //Go through each image and add to storage if already not there
+        for (int i = 0; i < images.length; i++) {
+            //File to check if already exists or not
+            File currImage = new File(targetPath,imageFileNames[i]);
+            //If image exists then do nothing
+            if(currImage.exists()){
+                System.out.println( imageFileNames[i] + " exists! - Do nothing");
+            }
+            else{
+                //Used to print to file - create image into file
+                fOut = new FileOutputStream(currImage);
+                //Sets bitmap to file
+                Bitmap bmp = BitmapFactory.decodeResource(getResources(), images[i]);
+
+                if (!bmp.compress(Bitmap.CompressFormat.JPEG, 90, fOut)) {
+                    Log.e("Log", "error while saving bitmap " + targetPath );
+                }
+                //Stop printing to file
+                fOut.close();
+                System.out.println("File did not exist - Was created");
+            }
+        }
+
+    }
+
+    /*
+        Method to check if the user's device has a memory card
+     */
+    private String checkForMemoryCard() {
+        String targetPath = "";
+
+        // Check for SD Card or internal storage
+        if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            System.out.println("No SD card");
+            Toast.makeText(this, "Error! No SDCARD Found!", Toast.LENGTH_LONG).show();
+            //status2.setText("No SD card/internal storage found, default images will be displayed instead");
+            return null;
+        }
+        else {
+            System.out.println("SD card found");
+            //Path to sd Card/internal strorage
+            String externalStoragePath = Environment.getExternalStorageDirectory().getAbsolutePath();
+            targetPath = externalStoragePath + "/DiscoverUCT";
+            return targetPath;
+        }
+    }
 
 
-}
+
+    }
