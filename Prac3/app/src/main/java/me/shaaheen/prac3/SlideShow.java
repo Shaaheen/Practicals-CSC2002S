079f589ccee29706d6db089e1bf0a694fe7a1ef7
Shaaheen Sacoor <sacoorshaheen@gmail.com>
2015-09-06 00:07:59 +0200

    Removed old files and changed text box color for buttons

--- Prac3/app/src/main/java/me/shaaheen/prac3/SlideShow.java
+++ Prac3/app/src/main/java/me/shaaheen/prac3/SlideShow.java
@@ -16,6 +16,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 /**
  * Created by Shaaheen on 9/2/2015.
